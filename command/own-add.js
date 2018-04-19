6cfea5dd6009f5d01c593890de13b52a84626fe2
ifredom <1950735817@qq.com>
2018-04-19 18:07:08 +0800

    fix delete

--- command/own-add.js
+++ command/own-add.js
@@ -1,31 +1,33 @@
 'use strict';
 const co = require('co');
 const prompt = require('co-prompt');
-const templates = require('../templates');
 const chalk = require('chalk');
 const fs = require('fs');
+const templates = require('../templates');
 
 module.exports = () => {
   co(function*() {
-    let tplName = yield prompt('Template name: ');
-    let gitUrl = yield prompt('Git UserName: ');
-    let branch = yield prompt('Branch: ');
+    let tplName = yield prompt('  Template name: ');
+    let gitUserName = yield prompt('  Git UserName: ');
+    let gitOwnProject = yield prompt('  Git User Project: ');
+    const branch = 'master';
 
     // 避免重复添加
     if (!templates[tplName]) {
       templates[tplName] = {};
-      templates[tplName]['branch'] = branch;
-      templates[tplName]['gitUrl'] = gitUrl.replace(/[\u0000-\u0019]/g, ''); // 过滤unicode字符
+      templates[tplName]['gitUserName'] = gitUserName;
+      templates[tplName]['gitOwnProject'] = gitOwnProject;
+      templates[tplName]['gitOwn'] = branch;
     } else {
-      console.log(chalk.red('Template has already existed!'));
+      console.log(chalk.red('  Template has already existed!'));
       process.exit();
     }
 
     // 把模板信息写入templates.json
     fs.writeFile(__dirname + '/../templates.json', JSON.stringify(templates), 'utf-8', err => {
       if (err) console.log(err);
-      console.log(chalk.green('New template added!\n'));
-      console.log(chalk.grey('The last template list is: \n'));
+      console.log(chalk.green('  New template added!\n'));
+      console.log(chalk.grey('  The last template list is: \n'));
       console.log(templates);
       console.log('\n');
       process.exit();
--- command/own-del.js
+++ command/own-del.js
@@ -1,29 +1,45 @@
 'use strict';
 const co = require('co');
-const prompt = require('co-prompt');
-const config = require('../templates');
+const coPrompt = require('co-prompt');
 const chalk = require('chalk');
+const { prompt } = require('inquirer');
 const fs = require('fs');
+const templates = require('../templates');
 
 module.exports = () => {
   co(function*() {
+    let tplName = yield coPrompt('  Will delete template\'s name: ');
 
-    let tplName = yield prompt('Template name: ');
-
-    if (config.tpl[tplName]) {
-      config.tpl[tplName] = undefined;
+    if (templates[tplName]) {
+      delete templates[tplName];
     } else {
-      console.log(chalk.red('Template does not exist!'));
+      console.log(chalk.red('  Templates does not exist!'));
       process.exit();
     }
 
-    fs.writeFile(__dirname + '/../templates.json', JSON.stringify(config), 'utf-8', err => {
-      if (err) console.log(err);
-      console.log(chalk.green('Template deleted!'));
-      console.log(chalk.grey('The last template list is: \n'));
-      console.log(config);
-      console.log('\n');
-      process.exit();
+    const question = [
+      {
+        name: 'yes',
+        message: `Are you confirm delete ${tplName}?`,
+        type: 'confirm',
+        default: false
+      }
+    ];
+    prompt(question).then(answers => {
+
+      if (answers.yes) {
+        fs.writeFile(__dirname + '/../templates.json', JSON.stringify(templates), 'utf-8', err => {
+          if (err) console.log(err);
+          console.log('\n');
+          console.log(chalk.green('  Templates deleted!\n'));
+          console.log(chalk.grey('  The last templates list is: \n'));
+          console.log(templates);
+          console.log('\n');
+          process.exit();
+        });
+      } else {
+        process.exit();
+      }
     });
   });
 };
